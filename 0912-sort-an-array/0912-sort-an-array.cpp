5437272383c4dc1d9d39f22c3c4600d1aabf2d2b
Aditya5608 <iadityasingh08@gmail.com>
2025-01-31 13:50:44 +0530

    Time: 4 ms (99.16%), Space: 70.9 MB (83.69%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums;
+        // int low=0;
+        // int high=nums.size();
+        // while(low<high){
+        //     int mid=(low+high)/2;
+
+        // }
+
+        // )
+    }
+};
