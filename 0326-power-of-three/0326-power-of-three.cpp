f86873e592956f256dd5f625a9ae61200e5d0347
Aditya5608 <iadityasingh08@gmail.com>
2025-03-04 22:26:12 +0530

    Time: 14 ms (5.49%), Space: 8.9 MB (77.77%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfThree(int n) {
         if(n<=0) return false;
         if(n==1) return true;
-        if(n%3==2) return false;
+        if(n%3!=0) return false;
         return  isPowerOfThree(n/3);
     }
 };
