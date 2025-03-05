2adc594b95f0a98f888b33a6ea6715dbec570146
Aditya5608 <iadityasingh08@gmail.com>
2025-03-05 19:47:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,6 +13,11 @@ public:
             prev1 = cur;
         }
 
-        return cur;     
+         return cur;   
+        // if(n==0) return 1;
+        // if(n==1) return 1;
+        // return climbStairs(n-1)+climbStairs(n-2);
+      
+       
     }
 };
