58c10c08ef55977b2f57b8bbb02eadede6ee0764
Aditya5608 <iadityasingh08@gmail.com>
2025-03-09 13:57:28 +0530

    Time: 0 ms (100%), Space: 28 MB (84.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -16,19 +16,19 @@ public:
         return candidate;}
     
 };*/
-sort(nums.begin(),nums.end());                    
-int n= nums.size();
-return nums[n/2];
+// sort(nums.begin(),nums.end());                    
+// int n= nums.size();
+// return nums[n/2];
 
-// int element,count=0;
-// for(int i=0;i<nums.size();i++){
-//     if(count==0){
-//         element=nums[i];
-//         count++;
-//     }
-//     else if(nums[i]==element) count++;
-//     else count--;
-// }
-// return element;
+int element,count=0;
+for(int i=0;i<nums.size();i++){
+    if(count==0){
+        element=nums[i];
+        count++;
+    }
+    else if(nums[i]==element) count++;
+    else count--;
+}
+return element;
 }
 };
