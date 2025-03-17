11d1c656e423c52faa8b3592e9d259f545146710
Aditya5608 <iadityasingh08@gmail.com>
2025-03-17 09:55:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
+        unordered_map<int,int> cache;
         if(n<=1) return n;
+        if(cache.find(n) !=cache.end()){
+            return cache[n];
+        }
+        else{
+            cache[n]=fib(n-1)+fib(n-2);
+            return cache[n];
+        }
         //if(n==0) return 1;
-        return fib(n-1)+fib(n-2);
+      //  return fib(n-1)+fib(n-2);
         // int a=0;
         // int b=1;
         // for(int i=2;i<=n;i++){
